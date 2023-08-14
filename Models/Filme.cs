638618bde505fad4e6283c3d768cf2174aced8d4
henriqueLebre <henrique_lebre@hotmail.com>
2023-08-14 17:39:06 -0300

    Inserindo regras para adicao de Filmes

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,10 +1,18 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
-
+    [Required(ErrorMessage = "O titulo do Filme é obrigatório")]
     public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do Filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pdoerá ultrapassar 50 caracteres.")]
     public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do Filme é obrigatória")]
+    [Range(70, 600, ErrorMessage = "A duração não deve ultrapassar mais de 600 minutos e nem ser menor que 70 minutos.")]
     public int Duracao   { get; set; }
 
 }

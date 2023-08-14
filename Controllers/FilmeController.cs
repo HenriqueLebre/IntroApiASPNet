e8a5b484863353a6212fef0db7ea4e3b1ce1f804
henriqueLebre <henrique_lebre@hotmail.com>
2023-08-14 17:15:07 -0300

    iniciando com metodo PUT

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Microsoft.AspNetCore.Mvc.Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+
+    }
+
+}
